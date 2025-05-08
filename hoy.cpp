229aa2db6b39c3e0f040e7dca8aa1e4f2875ae6f
10312006 <hrt200631@gmail.com>
2025-05-08 14:00:30 -0600

    Add files via upload

---
+++ hoy.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ #include <string.h> 
+int main(){
+
+int len;
+
+char origen[]="brisas";
+
+char destino [11] = "para";
+
+strcat(destino, origen);
+
+printf("destino: %s", destino);
+
+return 0;
+}
