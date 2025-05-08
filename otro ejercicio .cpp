d6b679f6835cee8eb40c27da0f3d9241c49d19f2
10312006 <hrt200631@gmail.com>
2025-05-08 14:01:15 -0600

    Add files via upload

---
+++ otro ejercicio .cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+int len;
+
+char cad[]="Es una cadena";
+
+len = strlen(cad);
+
+printf("La longitud de: \'%s\' es;%d\n", cad, len);
+
+
+return 0;
+}
